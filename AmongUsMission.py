2b85346e1c636ff95ed917ba06dc554a6da0c721
Prithvi Sathish <prithvi.mar05@gmail.com>
2020-10-23 18:57:17 +0530

    Removed Colon

--- AmongUsMission.py
+++ AmongUsMission.py
@@ -1,12 +1,11 @@
 import discord
 import asyncio
 from discord.ext import commands
-from discord.utils import get
-
 
 PREFIX = "$"
 bot = commands.Bot(command_prefix=PREFIX)
 client = discord.Client()
+
 
 @bot.event
 async def on_ready():
@@ -15,13 +14,11 @@ async def on_ready():
     return
 
 
-
 @bot.command()
-async def Au(ctx, arg):
-    channel = bot.get_channel(751460282118963201)
+async def Au(ctx, arg, arg2):
+    channel = bot.get_channel(768771698123210782)
     try:
-        Line = arg
-        Room_ID, Server = Line.split(":")
+        Room_ID, Server = arg, arg2
         await ctx.send("{} invite created successfully".format(ctx.message.author.mention))
         await channel.send("<@&757163360541474818> Your friend has invited you for a game of Among Us! Join soon!")
         await channel.send("Room ID: " + Room_ID)
@@ -45,18 +42,19 @@ async def Au(ctx, arg):
             else:
                 await channel.send("Oops, there are only " + str(count - 1) + " members! We can't play :(")
 
-
     except ValueError:
-        await ctx.send("Invalid invite\n1. Check if there is a colon in between the Room ID and Server\n2. Please type $cmd for commands")
+        await ctx.send(
+            "Invalid invite\n1. Check if there is a colon in between the Room ID and Server\n2. Please type $cmd for "
+            "commands")
 
 
 @bot.command()
 async def Pu(ctx, arg):
     if arg == "call":
-        channel = bot.get_channel(751460282118963201)
+        channel = bot.get_channel(768771698123210782)
         await ctx.send("{} Sure, just u wait".format(ctx.message.author.mention))
-        await channel.send("<@&757163360541474818> Your friend has invited you for a game of Among Us! Make sure to join!")
-
+        await channel.send(
+            "<@&757163360541474818> Your friend has invited you for a game of Among Us! Make sure to join!")
 
         # Get the message and the reaction
         msg = await channel.send("Are u guys comin? (react to the message)")
@@ -76,14 +74,17 @@ async def Pu(ctx, arg):
                 await channel.send("Oops, there are only " + str(count - 1) + " members! We can't play :(")
 
 
-
 @bot.command()
 async def cmd(ctx):
-    await ctx.send("Hello {}! I am the Among Us bot. Tadaaa! You could use me while calling your friends for a game of Among Us! "
-                    "But how do you call me? Here's how:\n1. Ask whether your friends are coming to play\n`$Pu call`\n "
-                    "The bot would wait for 20 mins for them to join. If it excedes, then it would inform you that there aren't enough members"
-                    "\n2. If there are enough members, then you could host the game\n`$Au *GameCode*:*Server*`\nThis would wait for 3 mins "
-                    "Then, you could start. Crewmates(or imposters) are supposed to join by reacting to the bot's message".format(ctx.message.author.mention))
+    await ctx.send(
+        "Hello {}! I am the Among Us bot. Tadaaa! You could use me while calling your friends for a game of Among Us! "
+        "But how do you call me? Here's how:\n1. Ask whether your friends are coming to play\n`$Pu call`\n "
+        "The bot would wait for 20 mins for them to join. If it excedes, then it would inform you that there aren't "
+        "enough members "
+        "\n2. If there are enough members, then you could host the game\n`$Au *GameCode* *Server*`\nThis would wait "
+        "for 3 mins "
+        "Then, you could start. Crewmates(or imposters) are supposed to join by reacting to the bot's message".format(
+            ctx.message.author.mention))
 
 
-bot.run('NzYyNjUwODc5NjUxNDc5NTYz.X3sP-w.T_SjWmlyHLhZnHDSV46dI1I2JcA')
+bot.run('TOKEN')
