22e7752d7055cbbe94fab4fc5a1c57acb0138898
Prithvi Sathish <prithvi.mar05@gmail.com>
2020-10-31 18:03:27 +0530

    Version 2

--- PollBot.py
+++ PollBot.py
@@ -9,14 +9,20 @@ Options = []
 
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="Make a Poll {Type $cmd for commands}", type=3)
+    activity = discord.Game(name="Make a Poll", type=3)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     return
+
+'''
+@bot.command()
+async def cmd(ctx):
+    await ctx.send("HOW TO START A POLL:")
+'''
 
 @bot.command()
 async def create(ctx, *args):
     global poll
-    poll = "Poll: " + ' '.join(args)
+    poll = "POLL: " + ' '.join(args)
     await ctx.send("Done")
 
 @bot.command()
@@ -28,6 +34,8 @@ async def add(ctx, *args):
 
 @bot.command()
 async def start(ctx):
+    Reactions = 0
+    Reaction_list = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
 
     if ctx.guild.name == "Let's Rock!":
         channel_id = 770898839473094687
@@ -36,20 +44,43 @@ async def start(ctx):
         channel_id = 762633165080100914
         Role_id = "<@&762632022547628042>"
     else:
-        channel_id = 751460282118963201
-        Role_id = "<@&757163360541474818>"
+        channel_id = 761971712613941268
+        Role_id = "<@&742311683585867869>"
 
     PollChannel = bot.get_channel(channel_id)
     try:
         await PollChannel.send(Role_id + " " + poll)
         for i in range(int(len(Options))):
-            msg = await PollChannel.send(str(i + 1) + ". " + Options[i])
-            await msg.add_reaction("👍")
-            await msg.add_reaction("👎")
+            Reactions += 1
+            await PollChannel.send(str(i + 1) + ". " + Options[i])
+            
+        msg = await PollChannel.send("React to the message according to the number to cast your poll")
+
+        for j in range(Reactions):
+            await msg.add_reaction(Reaction_list[j])
 
     except NameError:
         await ctx.send("Please create the poll first")
 
 
 
-bot.run('TOKEN')
+
+@bot.command()
+async def reason(ctx, arg1, arg2):
+
+    if arg2 == "admin":
+        role = "<@&675204055810834432>"
+        statement = "Being an " + role + ", this is considered a serious crime!"
+    if arg2 == "helping-hand":
+        role = "<@&675214370346893353>"
+        statement = "Being a " + role + ", its a shame on you!"
+    if arg2 == "channel-head":
+        role = "<@&749856631390732379>"
+        statement = "Being a " + role + ", a wrong example is being set to others!"
+
+    if arg1 == "diffmem":
+        await ctx.send("According to Rule: 1, adding members who do not belong here is crime! " + statement)
+
+
+
+bot.run('NzcwODk1MjI5NDg1MTg3MDgz.X5kOIQ.w_gx5uLJ734nkXL0bjnzPGcWP3M')
